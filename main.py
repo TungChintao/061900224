ab8cc771b50995568969b2f00a22d3b1e32fdb1d
JosephTong <1341033402@qq.com>
2021-09-14 09:10:24 +0800

    增加拼音检测功能

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import copy
 import re
-from pypinyin import pinyin, lazy_pinyin, Style
+from pypinyin import lazy_pinyin
 
 
 class DFA:
@@ -20,14 +20,15 @@ class DFA:
             length = len(keyword)
 
             for index, ch in enumerate(keyword):
-                char = lazy_pinyin(ch)[0]
-                if char not in current_dict:
-                    next_dict = {"is_end": False}
-                    current_dict[char] = next_dict
-                    current_dict = next_dict
-                else:
-                    next_dict = current_dict[char]
-                    current_dict = next_dict
+                char_word = lazy_pinyin(ch)[0]
+                for char in char_word:
+                    if char not in current_dict:
+                        next_dict = {"is_end": False}
+                        current_dict[char] = next_dict
+                        current_dict = next_dict
+                    else:
+                        next_dict = current_dict[char]
+                        current_dict = next_dict
 
                 if index == length - 1:
                     current_dict["is_end"] = keyword
@@ -40,36 +41,41 @@ class DFA:
         temp_match_list = []
 
         for char_pos, char in enumerate(content):
-            temp_char = lazy_pinyin(char.lower())[0]
+            temp_word = lazy_pinyin(char.lower())[0]
+            first = True
             if char == '\n':
                 self.line += 1
-            if temp_char in self.state_event_dict:
-                state_list.append(self.state_event_dict)
-                temp_match_list.append({
-                    "Line": self.line,
-                    "match": "",
-                    "word": ""
-                })
 
-            for index, state in enumerate(state_list):
-                if temp_char in state:
-                    state_list[index] = state[temp_char]
-                    temp_match_list[index]["word"] += char
+            for temp_char in temp_word:
+                if temp_char in self.state_event_dict:
+                    state_list.append(self.state_event_dict)
+                    temp_match_list.append({
+                        "Line": self.line,
+                        "match": "",
+                        "word": ""
+                    })
 
-                    if state[temp_char]["is_end"] is not False:
-                        temp_match_list[index]["match"] += state[temp_char]["is_end"]
-                        match_list.append(copy.deepcopy(temp_match_list[index]))
-                        self.total += 1
+                for index, state in enumerate(state_list):
+                    if temp_char in state:
+                        state_list[index] = state[temp_char]
+                        if first:
+                            temp_match_list[index]["word"] += char
+                            first = False
 
-                        if len(state[temp_char].keys()) == 1:
-                            state_list.pop(index)
-                            temp_match_list.pop(index)
-                elif re.match("[a-zA-Z\u4300-\u9fa5]", char, re.I) is None and temp_match_list[index]["word"] != '':
-                    temp_match_list[index]["word"] += char
+                        if state[temp_char]["is_end"] is not False:
+                            temp_match_list[index]["match"] += state[temp_char]["is_end"]
+                            match_list.append(copy.deepcopy(temp_match_list[index]))
+                            self.total += 1
 
-                else:
-                    state_list.pop(index)
-                    temp_match_list.pop(index)
+                            if len(state[temp_char].keys()) == 1:
+                                state_list.pop(index)
+                                temp_match_list.pop(index)
+                    elif re.match("[a-zA-Z\u4300-\u9fa5]", char, re.I) is None and temp_match_list[index]["word"] != '':
+                        temp_match_list[index]["word"] += char
+
+                    else:
+                        state_list.pop(index)
+                        temp_match_list.pop(index)
 
         return match_list
 
@@ -90,7 +96,7 @@ if __name__ == "__main__":
     f_ans = open(argv_list[3], 'w', encoding='utf-8', )
     f_ans.write('Total: ' + str(dfa.total) + '\n')
     for i in ans:
-        f_ans.write('Line'+str(i['Line'])+': '+'<' + str(i['match']) + '> '+str(i['word']) + '\n')
+        f_ans.write('Line' + str(i['Line']) + ': ' + '<' + str(i['match']) + '> ' + str(i['word']) + '\n')
     f_ans.close()
 
 
