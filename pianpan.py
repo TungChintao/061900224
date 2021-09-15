9816570ba8b1c7f32cf05827486182e33aaf781a
JosephTong <1341033402@qq.com>
2021-09-16 00:13:56 +0800

    优化文件路径获取方法’

--- pianpan.py
+++ pianpan.py
@@ -1,10 +1,13 @@
 import pickle
 import os
+# import pkg_resources
 
 
 class Pianpan(object):
     def __init__(self):
-        self.path = os.getcwd() + '\data.pkl'
+        # self.path = pkg_resources.resource_filename(__name__, "data.pkl")
+        # self.path = os.getcwd() + '/data.pkl'
+        self.path = os.path.dirname(__file__) + '/data.pkl'
         with open(self.path, 'rb') as f:
             self.data = pickle.load(f)
 
@@ -18,12 +21,11 @@ class Pianpan(object):
             result = input_char
         return result
 
-
-if __name__ == "__main__":
-    pp = Pianpan()
-    c = '里'
-    a = '面'
-    result = pp.toPianpan(a)
-    print(a == c)
-
-    print(result)
+#
+# if __name__ == "__main__":
+#     pp = Pianpan()
+#     c = '里'
+#     a = '你'
+#     result = pp.toPianpan(a)
+#
+#     print(result)
